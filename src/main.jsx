b2dd8f5c45b8f06475cbef0ba8a4ab316e0a7434
Jay Shiurkar <71687356+jayshiurkar@users.noreply.github.com>
2026-04-22 12:47:35 -0400

    Add lightweight route handling for SCM project page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SCMProject from "./scmProject.jsx";
+
+const path = window.location.pathname;
+
+function RootRouter() {
+  if (path.includes("/projects/scmprj")) {
+    return <SCMProject />;
+  }
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RootRouter />
   </React.StrictMode>
 );
